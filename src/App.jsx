e58eda8e71da5a1ed7ec730186e6a51e235e4ede
ankit jangid <martinxaj@gmail.com>
2025-01-21 21:40:52 +0530

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { Home } from "./pages/Home";
 import OpenRoute from "./components/Auth/OpenRoute";
 import HeaderLayout from "./components/common/HeaderLayout";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { user } = useSelector((state) => state.profile);
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<HeaderLayout />}>
+        <Route
+          path="/"
+          element={user!=null ? <Navigate to="/login" /> : <Navigate to="/homepage" />}
+        />
           <Route
             path="/signup"
             element={
